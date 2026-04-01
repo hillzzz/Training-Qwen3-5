3b3c506bc08b0d59c29d02b327a1d63954d85637
hillzzz <zen71001@zen.co.uk>
2026-04-01 23:27:27 +0100

    Changed env w/ pip & more...
    - Added pip packages: ipywidgets, unsloth==2026.3.6,
    unsloth_zoo==2026.3.6, trl<=0.24.0, transformers==5.2.0,
    tokenizers>=0.21.0, bitsandbytes>=0.45.0, triton>=3.3.0,
    accelerate>=0.26.0, flash-linear-attention
    - Added 38 files
    - Modified files: preBuild.bash, requirements.txt

--- preBuild.bash
+++ preBuild.bash
@@ -3,3 +3,10 @@
 # before any system packages or programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
+pip config set global.extra-index-url https://pypi.ngc.nvidia.com
+pip config set global.trusted-host pypi.ngc.nvidia.com
+
+pip install torch torchvision torchaudio \
+    --extra-index-url https://pypi.ngc.nvidia.com \
+    --upgrade \
+    --quiet
